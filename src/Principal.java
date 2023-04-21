096beb5ebda812016abb80acec95aa022ee4085f
LUAN BRENDON <102001821+luanbrendon@users.noreply.github.com>
2023-04-21 16:20:46 -0300

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
 
     public static void main(String args[]) {
         try {
-            Socket servidor = new Socket("10.150.9.174", 4444);//endereco e porta
+            Socket servidor = new Socket("localhost", 4444);//endereco e porta
             PrintWriter out = new PrintWriter(servidor.getOutputStream(), true);
 //Executa uma Thread para receber as mensagens de retorno
             Cliente in = new Cliente(servidor);
